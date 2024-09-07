efb618f791cbfc8febe51a01973c21fe99e31c46
Ammaralh <ammaralhannafi@hotmail.com>
2024-09-08 01:16:28 +0200

    chore: Update sweep configuration for finetuning TD3 models

--- td3/finetune_slurm/td3_sweep_slurm_jobs.bash
+++ td3/finetune_slurm/td3_sweep_slurm_jobs.bash
@@ -1,25 +1,28 @@
 #!/bin/bash
-#SBATCH --job-name=td3-equi-3                 # Job name
+#SBATCH --job-name=td3-equi-8                # Job name
 #SBATCH --partition=main                      # Partition
 #SBATCH --output=logs-slurm/td3/slurm_%j.out  # SLURM and script STDOUT
 #SBATCH --error=logs-slurm/td3/slurm_%j.err   # SLURM and script STDERR
-#SBATCH --gres=gpu:1                          # Request 1 GPU
+#SBATCH --gres=gpu:1                        # Request 1 GPU
 #SBATCH --ntasks=1                            # Run 1 task at a time
-#SBATCH --cpus-per-task=4                     # Allocate CPUs per task (adjust based on your needs)
+#SBATCH --cpus-per-task=8                    # Allocate CPUs per task (adjust based on your needs)
 
 # Show actual node in output file, useful for diagnostics
-hostname
+echo "Starting job on $(hostname) at $(date)"
+echo "Running my analysis..."
 
 # Load the necessary modules
 source /etc/profile.d/modules.sh
-module load anaconda3/2024.02
 module load nvidia/cuda-12.4
-
-# Activate the conda environment
-source ~/.bashrc
+module load anaconda3/2023.09
+source activate
+# Activate the Conda environment
 conda init bash
+source ~/.bashrc 
 conda activate equivariance-rl
 
-
 #/home/s2657708/.conda/envs/equivariance-rl/bin/python td3/td3_optuna.py
-/home/s2657708/.conda/envs/equivariance-rl/bin/wandb agent ammaralh/Equivariant_TD3_InvertedPendulum/8si6nlqw
+#/home/s2657708/.conda/envs/equivariance-rl/bin/wandb agent ammaralh/Equivariant_TD3_InvertedPendulum/8si6nlqw
+# echo which python
+python -c "import sys; print(f'Using Python interpreter at: {sys.executable}')"
+wandb agent ammaralh/Equivariant_TD3_InvertedPendulum/j954574a
